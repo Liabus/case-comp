a3342c38278c72f5605bc7b67ec48ee71068d264
Jordan Gensler <jordangens@gmail.com>
2014-03-03 21:24:13 -0600

    Starting development work on jobs page. Main page redesign implemented. Now starting work on linking things.

--- app/scripts/controllers/jobs.js
+++ app/scripts/controllers/jobs.js
@@ -6,54 +6,121 @@ angular.module('caseCompApp')
       $scope.jobs = [];
       $scope.jobModel = {};
       
+      
+      
+      
+      
+      
+      $scope.sortMode = 'name';
+      $scope.sortString = 'Name';
+      //Does the opposite of what it sounds like:
+      $scope.sortDesc = false;
+      $scope.sortBackwards = false;
+      
+      $scope.searchMode = 'all';
+      $scope.searchString = 'All';
+      
       if($routeParams.id){
           $scope.edit = true;
-          Jobs.get({id: $routeParams.id}, function(res){
-              if(!res){
+      }
+      
+      var updateData = function(){
+          NProgress.start();
+          if($scope.edit){
+              Jobs.get({id: $routeParams.id}, function(res){
+                  NProgress.done();
+                  $scope.um = res;
+              }, function(){
+                  NProgress.done();
                   $location.path('/jobs');
-                  return;
-              }
-              $scope.jobModel = res;
-          }, function(){
-              $location.path('/jobs');
-          })
-      }
-      
-      Jobs.list(function(jobs){
-          $scope.jobs = jobs.jobs;
-      });
-      
-      $scope.addJobLabel = function(){
-          if($scope.edit){
-              return 'Edit Job';
-          }
-          return 'Add Job';
-      }
-      
-      $scope.addJob = function(){
-          if($scope.edit){
-              //Being a Jabrone.
-              //It's the best thing ever! Jabrones for life. I Jabrone, you Jabrone, he/she/it Jabrones. Jabronology, the study of Jabron.
-              Jobs.update($scope.jobModel,
-                  function(user) {
-                      $location.path('/jobs/view/' + $scope.jobModel._id);
-                  },
-                  function(err) {
-                      console.log(err);
-                      alert('An error occured while saving.');
-                  }
-              );
+              });
           }else{
-            Jobs.save($scope.jobModel,
-                function(user) {
-                    $location.path('/jobs');
-                },
-                function(err) {
-                    console.log(err);
-                    alert('An error occured while saving.');
-                }
-            );
-        }
-      };
+              Jobs.list(function(jobs){
+                  NProgress.done();
+                  $scope.jobs = jobs.jobs;
+              });
+          }
+      }
+      
+      updateData();
+      
+      $scope.deArray = function(det, first){
+          if(_.isArray(det)){
+              if(first){
+                  return det[0];
+              }
+              return det.join(', ');
+          }
+          return det;
+      }
+      
+      $scope.pickRelevant = function(job){
+          var sm = $scope.sortMode.toLowerCase();
+          if(sm === 'name') sm = '';
+          
+          var rel = sm || (job.matched || '').toLowerCase() || 'name';
+          
+          if(rel === 'name'){
+              return job.location;
+          }else{
+              return job[rel] || job.location || '';
+          }
+      }
+      
+      $scope.swapSortDirection = function(){
+          $scope.sortDesc = !$scope.sortDesc;
+      }
+      $scope.setSort = function(mode, str, force){
+          $scope.sortMode = mode;
+          $scope.sortString = str;
+          $scope.sortBackwards = force || false;
+      }
+      $scope.getSortDesc = function(){
+          if($scope.sortBackwards){
+              return !$scope.sortDesc;
+          }
+          return $scope.sortDesc;
+      }
+      
+      $scope.clearSearch = function(){
+          $scope.searchMode = 'all';
+          $scope.searchString = 'All';
+          $scope.search = '';
+      }
+      $scope.setSearch = function(mode, str){
+          $scope.searchMode = mode;
+          $scope.searchString = str;
+      }
+      
+      $scope.deleteClicked = function(){
+          var modalInstance = $modal.open({
+            templateUrl: 'partials/deleteModal.html',
+            controller: 'DeleteModalController',
+            resolve: {
+              type: function () {
+                  return 'Job';
+              },
+              name: function () {
+                  return $scope.jobModel.name;
+              }
+            }
+          });
+
+          modalInstance.result.then(function (resAction) {
+              if(resAction === 'yes'){
+                  NProgress.start();
+                  Jobs.delete({id: $routeParams.id}, function(res){
+                      NProgress.done();
+                      $location.path('/jobs');
+                  }, function(err){
+                      console.log(err);
+                      NProgress.done();
+                      alert('An unknown error occured while deleting the job.');
+                  });
+              }
+          }, function () {
+              //Nope.
+          });
+      }
       
   });
--- app/scripts/directives/megaFilter.js
+++ app/scripts/directives/megaFilter.js
@@ -68,4 +68,73 @@ angular.module('caseCompApp')
             return filtered;
             
         }
-     });
+     })
+     
+     .filter('megaJobsFilter', function () {
+        
+         var searchString = function(val, query){
+             if(_.isArray(val)){
+                 val = val.join(', ');
+             }
+             return (val.toLowerCase().indexOf(query.toLowerCase()) >= 0);
+         }
+        
+         return function(items, searchText) {
+            
+             //First, reset all matched queries: 
+             angular.forEach(items, function(item) {
+                 item.matched = '';
+             });
+            
+             if(!searchText || !searchText.query){
+                 return items;
+             }
+            
+             var query = searchText.query || '';
+             var type = searchText.type || 'all';
+            
+             var filtered = [];
+            
+            
+             angular.forEach(items, function(item) {
+                 if(type === 'name' || type === 'all'){
+                     if(searchString(item.name, query)){
+                         item.matched = 'name';
+                         filtered.push(item);
+                         return;
+                     }
+                 }
+                 if(type === 'university' || type === 'all'){
+                     if(searchString(item.university, query)){
+                         item.matched = 'university';
+                         filtered.push(item);
+                         return;
+                     }
+                 }
+                 if(type === 'major' || type === 'all'){
+                     if(searchString(item.major, query)){
+                         item.matched = 'major';
+                         filtered.push(item);
+                         return;
+                     }
+                 }
+                 if(type === 'minor' || type === 'all'){
+                     if(searchString(item.minor, query)){
+                         item.matched = 'minor';
+                         filtered.push(item);
+                         return;
+                     }
+                 }
+                 if(type === 'gpa' || type === 'all'){
+                     if(+query <= +item.GPA){
+                         item.matched = 'GPA';
+                         filtered.push(item);
+                         return;
+                     }
+                 }
+             });
+            
+             return filtered;
+            
+         }
+      });
