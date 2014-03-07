0ac9bc0980f1e17eac1ce84b447f2aeb9e48a5cd
Jordan Gensler <jordangens@gmail.com>
2014-03-07 02:41:39 -0600

    Update Express + done with uploads.

--- app/scripts/controllers/modal.js
+++ app/scripts/controllers/modal.js
@@ -18,10 +18,8 @@ angular.module('caseCompApp')
           //console.log('uploading....')
         };
         $scope.uploadComplete = function (content) {
-          console.log(content);
-          content = JSON.parse(content || '');
-          if(content.url){
-            $scope.model.resumeUrl = content.url;
+          if(content.data){
+            $scope.model.resumeUrl = content.data;
             alert('Resume has been uploaded and added to the candidate.');
           }
         }
